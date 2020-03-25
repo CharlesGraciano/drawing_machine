01cb19aa378a0bbb4d6f98ecf58d793b97e75124
Pandora's Box <55153290+CharlesGraciano@users.noreply.github.com>
2020-03-25 15:17:01 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,19 @@
 function setup() {
   createCanvas(600, 1000);
-  background(255, 30, 115);
-  srtokeWeight(5);
+  background(5, 89, 470);
+  strokeWeight(25);
 
 }
 
 function draw() {
 
 if (mouseIsPressed==true){
-	stroke(map(mouseX,0,600,0,255,true))
+	stroke(map(mouseX,0,500,0,300,true));
 	line(width-mouseX, height-mouseY, width-pmouseX, height-pmouseY);
 	line(mouseX, mouseY, pmouseX, pmouseY);
 
 
 }
   
+
 }
